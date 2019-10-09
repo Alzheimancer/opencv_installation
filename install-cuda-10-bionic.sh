48cb1363b21f68e2dbe605b5d29bcfa53fc3b4af
tk_data <prissadang@kdlab.ai>
2019-10-09 17:33:12 +0700

    update version

--- install-cuda-10-bionic.sh
+++ install-cuda-10-bionic.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 1. Install CUDA Toolkit 10
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub && sudo apt update
 sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 
@@ -21,6 +22,6 @@ sudo apt upgrade
 # Link libraries to standard locations
 sudo mkdir -p /usr/local/cuda-10.0/nccl/lib
 sudo ln -s /usr/lib/x86_64-linux-gnu/libnccl.so.2 /usr/local/cuda/nccl/lib/
-sudo ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so.7 /usr/local/cuda-10.0/lib64/
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so.7 /usr/local/cuda-10.1/lib64/
 
 echo 'If everything worked fine, reboot now.'
